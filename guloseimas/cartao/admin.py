266be6c7bcbbbbc7ccc7745ec0b678ce3720513f
Gustavo <gustavogbi@hotmail.com>
2019-10-26 14:06:06 -0300

    :hammer: Retira User e Group do Django Admin

--- guloseimas/cartao/admin.py
+++ guloseimas/cartao/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Cartao
+from django.contrib.auth.models import User, Group
+
+# Unregister
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
 # Register your models here.
 admin.site.register(Cartao)
