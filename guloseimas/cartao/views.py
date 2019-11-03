10d6257e709977a801bbb0d76e7a9a4646a51187
Gustavo <gustavogbi@hotmail.com>
2019-11-03 15:12:44 -0300

    :rocket: Cria view para listar os cartões

--- guloseimas/cartao/views.py
+++ guloseimas/cartao/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Cartao
 
-# Create your views here.
+def mostrar_cartoes(request):
+  cartoes = Cartao.objects.all()
+  response = []
+  for cartao in cartoes:
+    response.append(cartao)
+    response.append("<br>")
+    response.append(cartao.saldo)
+    response.append("<br>")
+  return HttpResponse(response)
